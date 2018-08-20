be68817b190963981352a8ad57413c838865c735
Jeppe Johansen <Jeppe@jeppes-mbp.home>
2018-08-20 14:57:38 +0200

    made dash_app and app work

--- python365/dash_app.py
+++ python365/dash_app.py
@@ -18,4 +18,4 @@ app.layout = html.Div([
 ])
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(debug=True, host='0.0.0.0')
--- scripts/build-docker.sh
+++ scripts/build-docker.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+###################################
+# Building the docker container
+###################################
+
+
 #be sure that we are in WebApp_dummy
 pwd
 
-#  conda env --> probably shouldn't be used
-
-# cp environment.yml anaconda/environment.yml
+# conda env --> probably shouldn't be used
 # docker build --tag conda ./anaconda
 
+source activate webapp
 pip freeze > python365/requirements.txt
+
+cd python365
+docker build -t flask-sample-one:latest .
+
+
+cd ..
